9f2f981d9bc9000faae1aba7864f49186b63628c
yjh0410 <1394571815@qq.com>
2021-07-28 16:20:24 +0800

    fix a bug in fcos

--- utils/box_ops.py
+++ utils/box_ops.py
@@ -1,10 +1,13 @@
 import torch
 
-
-def iou_score(bboxes_a, bboxes_b):
+def iou_score(bboxes_a, bboxes_b, batch_size):
     """
-        bbox_1 : [B*N, 4] = [x1, y1, x2, y2]
-        bbox_2 : [B*N, 4] = [x1, y1, x2, y2]
+        Input:\n
+        bboxes_a : [B*N, 4] = [x1, y1, x2, y2] \n
+        bboxes_b : [B*N, 4] = [x1, y1, x2, y2] \n
+
+        Output:\n
+        iou : [B, N] = [iou, ...] \n
     """
     tl = torch.max(bboxes_a[:, :2], bboxes_b[:, :2])
     br = torch.min(bboxes_a[:, 2:], bboxes_b[:, 2:])
@@ -13,10 +16,12 @@ def iou_score(bboxes_a, bboxes_b):
 
     en = (tl < br).type(tl.type()).prod(dim=1)
     area_i = torch.prod(br - tl, 1) * en  # * ((tl < br).all())
-    return area_i / (area_a + area_b - area_i + 1e-20)
+    iou = area_i / (area_a + area_b - area_i + 1e-14)
+
+    return iou.view(batch_size, -1)
 
 
-def giou_score(bboxes_a, bboxes_b):
+def giou_score(bboxes_a, bboxes_b, batch_size):
     """
         bbox_1 : [B*N, 4] = [x1, y1, x2, y2]
         bbox_2 : [B*N, 4] = [x1, y1, x2, y2]
@@ -29,7 +34,7 @@ def giou_score(bboxes_a, bboxes_b):
 
     en = (tl < br).type(tl.type()).prod(dim=1)
     area_i = torch.prod(br - tl, 1) * en  # * ((tl < br).all())
-    iou = (area_i / (area_a + area_b - area_i)).clamp(0)
+    iou = (area_i / (area_a + area_b - area_i + 1e-14)).clamp(0)
     
     # giou
     tl = torch.min(bboxes_a[:, :2], bboxes_b[:, :2])
@@ -37,9 +42,9 @@ def giou_score(bboxes_a, bboxes_b):
     en = (tl < br).type(tl.type()).prod(dim=1)
     area_c = torch.prod(br - tl, 1) * en  # * ((tl < br).all())
 
-    giou = (iou - (area_c - area_i) / area_c)
+    giou = (iou - (area_c - area_i) / (area_c + 1e-14))
 
-    return giou
+    return giou.view(batch_size, -1)
 
 
 if __name__ == '__main__':
