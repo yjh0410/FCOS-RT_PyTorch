c42e6a5209a9abed7ce94eeba87f4b61d8437dc3
yjh0410 <1394571815@qq.com>
2021-10-02 13:55:51 +0800

    update

--- test.py
+++ test.py
@@ -154,7 +154,7 @@ if __name__ == '__main__':
     
 
     # load weight
-    net.load_state_dict(torch.load(args.trained_model, map_location=device), strict=False)
+    net.load_state_dict(torch.load(args.trained_model, map_location=device))
     net.to(device).eval()
     print('Finished loading model!')
 
